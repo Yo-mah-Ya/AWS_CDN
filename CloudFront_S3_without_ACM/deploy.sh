b94ed03b2b705c53565bb16f4fcba57db98b0645
Yoshinari.Yamanaka <shanzhongziye@gmail.com>
2021-06-05 16:05:00 +0900

    feat: initial commit

---
+++ CloudFront_S3_without_ACM/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+profile="default"
+STACK_NAME="CDN"
+
+aws cloudformation deploy \
+    --template-file template.yml \
+    --stack-name ${STACK_NAME} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --profile ${profile}
